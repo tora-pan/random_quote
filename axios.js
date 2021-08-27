87d41358ce16f9f07bdaab7f2134d2c6092eccdb
tora_pan <travis.pandos@gmail.com>
2021-08-26 22:34:14 -0700

    add bootstrap, fix api fetching

--- axios.js
+++ axios.js
@@ -1,13 +1,16 @@
 const refreshBtn = document.getElementById("refresh-button");
+const author = document.getElementById("author");
+const quote = document.getElementById("quote");
 
 const getData = () => {
   console.log("getting clicked");
   axios.defaults.headers.post["Content-Type"] =
     "application/x-www-form-urlencoded";
-  axios.get("https://zenquotes.io/api/random").then((response) => {
+  axios.get("https://api.fisenko.net/quotes?l=en").then((response) => {
     console.log(response);
+    author.innerHTML = response.data.author;
+    quote.innerHTML = response.data.text;
   });
 };
 
 refreshBtn.addEventListener("click", getData);
-
